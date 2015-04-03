1d656328458d168974078c089365cd0940cf88dc
David Ko <david.dk.ko@gmail.com>
2015-04-02 17:33:31 -0700

    Added 32-bit and 64-bit package creation scripts

---
+++ chlinkbot_src/createpkg32.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+PACKAGE=chbarobo
+VERSION=1.0.0
+ARCH=win32
+PKGDIR=$PACKAGE-$VERSION-$ARCH/$PACKAGE
+
+echo Building $PACKAGE-$VERSION-$ARCH.zip ...
+rm -rf $PACKAGE-$VERSION-$ARCH
+rm -rf $PACKAGE-$VERSION-$ARCH.zip
+mkdir -p $PKGDIR
+mkdir $PKGDIR/lib
+mkdir $PKGDIR/demos
+mkdir $PKGDIR/dl
+mkdir $PKGDIR/include
+mkdir $PKGDIR/docs
+mkdir $PKGDIR/bin
+cp chlinkboti.chf $PKGDIR/lib
+cp chlinkbotl.chf $PKGDIR/lib
+cp liblinkbot.dl $PKGDIR/dl
+cp linkbot.h $PKGDIR/include
+cp dlls/$ARCH/* $PKGDIR/bin
+zip -rq $PACKAGE-$VERSION-$ARCH.zip $PACKAGE-$VERSION-$ARCH
